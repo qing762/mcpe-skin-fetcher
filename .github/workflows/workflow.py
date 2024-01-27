daf265972aadf3e76408ac4765684252365afaba
qing <threatedblade@outlook.com>
2024-01-27 23:29:39 +0800

    Change script to continue instead of exiting

--- .github/workflows/workflow.py
+++ .github/workflows/workflow.py
@@ -1,5 +1,4 @@
 import requests
-import sys
 from datetime import datetime
 
 ignx = "rekrap2"  # JUST FOR WORKFLOW PURPOSES
@@ -11,7 +10,8 @@ for ign in result:
     modifiedign = ign.replace(" ", "%20")
     r = requests.get(f"https://api.geysermc.org/v2/xbox/xuid/{modifiedign}")
     if r.status_code != 200:
-        sys.exit(f"\nIGN {ign} not found. Exiting...")
+        print(f"\nIGN {ign} not found. Skipping...")
+        continue
     else:
         xuid = r.json()["xuid"]
         r = requests.get(f"https://api.geysermc.org/v2/skin/{xuid}")
@@ -30,9 +30,10 @@ for ign in result:
                 "Skin URL": f"https://textures.minecraft.net/texture/{r.json()['texture_id']}",
             }
         except KeyError:
-            sys.exit(
-                "API returned with a blank json. You might wanna check if the player exist. Exiting..."
+            print(
+                "\nAPI returned with a blank json. You might wanna check if the player exist. Skipping..."
             )
+            continue
         print(f"\nPlayer {skin_result['Player']} found!\nXUID: {skin_result['XUID']}\n")
         print(
             f"Hash: {skin_result['Hash']}\nIs steve: {skin_result['Is steve']}\nLast modified: {skin_result['Last modified']}\nSignature: {skin_result['Signature']}\nTexture ID: {skin_result['Texture ID']}\nValue: {skin_result['Value']}\n"
