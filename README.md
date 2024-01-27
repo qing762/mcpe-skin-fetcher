48170dbe01bb6030213a7019edda9d2198eea530
qing <threatedblade@outlook.com>
2024-01-27 23:07:25 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,53 @@
+> [!NOTE]  
+> Join the [Discord server](https://qing762.is-a.dev/discord) for issues. Thanks a lot!
+
+> [!WARNING]
+> Please be advised that usage of this tool is entirely at your own risk. I assumes no responsibility for any adverse consequences that may arise from its use, and users are encouraged to exercise caution and exercise their own judgment in utilizing this tool.
+# MCPE Skin Fetcher
+
+Fetch MCPE user skin through Geyser API by Xbox IGN.
+
+
+
+
+## How it works
+
+[Here's a guide for it.](https://www.youtube.com/watch?v=DKhBCFU5AHw) This script is just the whole process of the video but it does automatically, as well as some improved functions.
+## Features
+
+- Able to fetch multiple users account at a time.
+- All automatically besides the username input only.
+- Error handling
+
+
+
+## Installation / Usage
+
+First, clone this repository
+```bash
+git clone https://github.com/qing762/mcpe-skin-fetcher/
+```
+
+Install the necessary dependencies:
+```bash
+pip install -r requirements.txt
+```
+
+Finally run the Python file:
+```bash
+python main.py
+```
+And you're all set! Follow the instructions while interacting with the Python file.
+## Contributing
+
+Contributions are always welcome!
+
+To contribute, fork this repository and improve it. After that, make a pull request.
+
+
+## Feedback / Issues
+
+If you have any feedback or issues running the code, please join the [Discord server](https://qing762.is-a.dev/discord)
+
+### FOR MOJANG AB EMPLOYEES / GEYSERMC STAFF:
+If the company wishes to discontinue or terminate this project, please do not hesitate to reach out to me on Discord. I can be reached at [Discord/qing762](https://discord.com/users/635765555277725696). Thank you for your attention to this matter.
---
+++ main.py
@@ -0,0 +1,43 @@
+import requests
+import sys
+from datetime import datetime
+
+ignx = input("Input the MCPE player name to search (Each divided by comma): ")
+result = [name.strip() for name in ignx.split(",")]
+
+skin_results = []
+
+for ign in result:
+    modifiedign = ign.replace(" ", "%20")
+    r = requests.get(f"https://api.geysermc.org/v2/xbox/xuid/{modifiedign}")
+    if r.status_code != 200:
+        sys.exit(f"\nIGN {ign} not found. Exiting...")
+    else:
+        xuid = r.json()["xuid"]
+        r = requests.get(f"https://api.geysermc.org/v2/skin/{xuid}")
+        try:
+            skin_result = {
+                "Player": ign,
+                "XUID": xuid,
+                "Hash": r.json()["hash"],
+                "Is steve": "No" if r.json()["is_steve"] == "false" else "Yes",
+                "Last modified": datetime.fromtimestamp(
+                    int(r.json()["last_update"]) / 1000
+                ),
+                "Signature": r.json()["signature"],
+                "Texture ID": r.json()["texture_id"],
+                "Value": r.json()["value"],
+                "Skin URL": f"https://textures.minecraft.net/texture/{r.json()['texture_id']}",
+            }
+        except KeyError:
+            sys.exit(
+                "\nAPI returned with a blank json. You might wanna check if the player exist. Exiting..."
+            )
+        print(f"\nPlayer {skin_result['Player']} found!\nXUID: {skin_result['XUID']}\n")
+        print(
+            f"Hash: {skin_result['Hash']}\nIs steve: {skin_result['Is steve']}\nLast modified: {skin_result['Last modified']}\nSignature: {skin_result['Signature']}\nTexture ID: {skin_result['Texture ID']}\nValue: {skin_result['Value']}\n"
+        )
+        skin_results.append(skin_result)
+
+for skin_result in skin_results:
+    print(f"Here is {skin_result['Player']}'s skin. Enjoy!\n{skin_result['Skin URL']}")
